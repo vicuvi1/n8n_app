b8b9399305aab60a4afa8bc76a5031428166b457
vicuvi1 <vicuvi1@users.noreply.github.com>
2026-06-28 23:32:11 +0300

    Fix config package exports for LLM_PROVIDERS import

---
+++ config/__init__.py
@@ -0,0 +1,27 @@
+"""App configuration package."""
+
+from config.constants import (
+    APP_ICON,
+    APP_TITLE,
+    AUTO_REFRESH_INTERVAL,
+    DEBUG,
+    EXECUTIONS_LIMIT,
+    GEMINI_MODEL,
+    GROQ_MODEL,
+    LLM_PROVIDERS,
+    N8N_REQUEST_TIMEOUT,
+    OPENAI_MODEL,
+)
+
+__all__ = [
+    "APP_ICON",
+    "APP_TITLE",
+    "AUTO_REFRESH_INTERVAL",
+    "DEBUG",
+    "EXECUTIONS_LIMIT",
+    "GEMINI_MODEL",
+    "GROQ_MODEL",
+    "LLM_PROVIDERS",
+    "N8N_REQUEST_TIMEOUT",
+    "OPENAI_MODEL",
+]
